77ede8152ec4f1c1ace765fe92721ece79204486
mathildekochlarsen-ui <mathilde.kochlarsen@gmail.com>
2026-04-27 09:56:03 +0200

    Initialize Git repository and create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+echo "# Trunkbaseddevelopment" >> app.py
+git init
+git add app.py
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/mathildekochlarsen-ui/Trunkbaseddevelopment.git
+git push -u origin main
